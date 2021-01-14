be9f9e4a936c96bef28805a7ed09cba6922642c3
Alejandro Linan <jandrolinan@gmail.com>
2021-01-13 21:20:46 -0600

    most initial display functions created - shows initial display and info - shows main menu and asks for user input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,12 @@
+import initialDisplays.Welcome;
 
 public class App {
 
+	public static void main(String[] args) {
+
+		Welcome.welcomeDisplay();
+		Welcome.mainMenuOptions();
+	
+	}
+	
 }
---
+++ src/main/java/initialDisplays/Welcome.java
@@ -0,0 +1,49 @@
+package initialDisplays;
+
+import java.util.Scanner;
+
+public class Welcome {
+	
+	static Scanner scanner = new Scanner(System.in);
+
+	public static void welcomeDisplay() {
+		System.out.println("==================");
+		System.out.println("==================");
+		System.out.println("Application: LockedMe.com");
+		System.out.println("Developer: Alejandro Linan");
+		System.out.println("Description: A Virtual Key for your Repositories. Here you can add, delete, and search your files and folders!");
+		System.out.println("==================");
+		System.out.println("==================");
+	}
+
+	public static void mainMenuOptions() {
+		System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+		System.out.println("MAIN MENU");
+		System.out.println("Instruction: Please input the number of the action you wish to take");
+		System.out.println("1. Show Files in Ascending Order");
+		System.out.println("2. View File Operations");
+		System.out.println("3. Close the Application");
+		System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+		
+		String chosenAction = scanner.nextLine();
+		handleMenuOptions(chosenAction);
+	}
+	
+	public static void handleMenuOptions(String input) {
+		switch(input) {
+			case "1":
+				System.out.println("Showing files in ascending order....");
+				break;
+			case "2":
+				System.out.println("Fetching operations for your command....");
+				break;
+			case "3":
+				System.out.println("Exiting application....");
+				break;
+			default:
+				System.out.println("Invalid input, please choose 1, 2, or 3");
+		}
+		mainMenuOptions();
+	}
+	
+}
