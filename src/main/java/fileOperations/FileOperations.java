7a232751b43c721c4c4a2e50936b7f2bd5a42be5
Alejandro Linan <jandrolinan@gmail.com>
2021-01-14 09:16:20 -0600

    handle operations method added, basic system outs listed but it can send back to main menu

--- src/main/java/fileOperations/FileOperations.java
+++ src/main/java/fileOperations/FileOperations.java
@@ -1,6 +1,12 @@
 package fileOperations;
 
+import initialDisplays.Welcome;
+
+import java.util.Scanner;
+
 public class FileOperations {
+	
+	static Scanner scanner = new Scanner(System.in);
 
 	public static void showFileOperations() {
 		System.out.println("------++++++============++++++------");
@@ -9,6 +15,30 @@ public class FileOperations {
 		System.out.println("3. Search for a file");
 		System.out.println("4. Back to main menu");
 		System.out.println("------++++++============++++++------");
+		
+		String chooseOperation = scanner.nextLine();
+		handleFileOperations(chooseOperation);
+	}
+	
+	public static void handleFileOperations(String action) {
+		switch(action) {
+			case "1":
+				System.out.println("Adding a file");
+				break;
+			case "2":
+				System.out.println("Deleting a file");
+				break;
+			case "3":
+				System.out.println("Searching a file");
+				break;
+			case "4":
+				System.out.println("Getting the Main Menu...");
+				Welcome.mainMenuOptions();
+				break;
+			default:
+				System.out.println("Invalid input, please input a 1, 2, 3, or 4");
+		}
+		showFileOperations();
 	}
 	
 }
